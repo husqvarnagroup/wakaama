88e56e6b490b32b6ef9c27961ce44a6b9a65f432
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2023-07-27 16:05:27 +0200

    Allow registration message without Resource Type
    Registration messages without `rt="oma.lwm2m"` should be
    accepted as valid request.

--- core/registration.c
+++ core/registration.c
@@ -1357,9 +1357,9 @@ static int prv_parseLinkAttributes(uint8_t * data,
     uint16_t index;
     uint16_t pathStart;
     uint16_t pathLength;
-    bool isValid;
+    uint8_t rtValCount;
 
-    isValid = false;
+    rtValCount = 0;
 
     // Expecting application/link-format (RFC6690)
     // leading space were removed before. Remove trailing spaces.
@@ -1407,13 +1407,13 @@ static int prv_parseLinkAttributes(uint8_t * data,
         if (keyLength == REG_ATTR_TYPE_KEY_LEN
          && 0 == lwm2m_strncmp(REG_ATTR_TYPE_KEY, (char*)data + index + keyStart, keyLength))
         {
-            if (isValid == true) return 0; // declared twice
             if (valueLength != REG_ATTR_TYPE_VALUE_LEN
              || 0 != lwm2m_strncmp(REG_ATTR_TYPE_VALUE, (char*)data + index + valueStart, valueLength))
             {
                 return 0;
             }
-            isValid = true;
+            ++rtValCount;
+            if (rtValCount > 1) return 0; // declared twice (or more times)
         }
         else if (keyLength == REG_ATTR_CONTENT_KEY_LEN
               && 0 == lwm2m_strncmp(REG_ATTR_CONTENT_KEY, (char*)data + index + keyStart, keyLength))
@@ -1446,7 +1446,7 @@ static int prv_parseLinkAttributes(uint8_t * data,
         index += result;
     }
 
-    if (isValid == false) return 0;
+    if (rtValCount > 1) return 0;
 
     if (pathLength != 0)
     {
