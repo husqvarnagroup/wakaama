eb05e518c249a7f3852b6679bfa348f7dfda3776
Lukas Woodtli <lukas.woodtli@husqvarnagroup.com>
2024-10-17 14:59:30 +0200

    rust: Generate wrapper code for Wakaama
    The code is generated with `bindgen` during the build with `cargo`.

--- wakaama-rs/build.rs
+++ wakaama-rs/build.rs
@@ -1,7 +1,10 @@
+use std::path::PathBuf;
+
 fn main() {
     let wakaama_c_path = "../";
 
     build_wakaama(wakaama_c_path);
+    generate_wrapper(wakaama_c_path);
 }
 
 fn build_wakaama(wakaama_c_path: &str) {
@@ -13,4 +16,19 @@ fn build_wakaama(wakaama_c_path: &str) {
         dst.join("build").display()
     );
     println!("cargo:rustc-link-lib=static=wakaama_static");
+}
+
+fn generate_wrapper(wakaama_c_path: &str) {
+    let c_header_path = PathBuf::from(wakaama_c_path).join("include/liblwm2m.h");
+    println!("cargo:rerun-if-changed={}", c_header_path.display());
+
+    let bindings = bindgen::Builder::default()
+        .header(c_header_path.to_str().unwrap())
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("wakaama_bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
